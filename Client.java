5d348bcc0df4b49568a4d9ecb31d60717ac85436
zhemin <forevermzm00@gmail.com>
2014-01-13 16:07:49 -0500

    Working solution without SSL

--- Client.java
+++ Client.java
@@ -1,29 +1,62 @@
 import java.net.*;
 import java.io.*;
 public class Client {
-    public static void main(String args[]) throws IOException {
-        Socket s1 = new Socket("cs5700.ccs.neu.edu",27993);
-        OutputStream s1Out = s1.getOutputStream();
-        DataOutputStream dos = new DataOutputStream (s1Out);
-		dos.writeChars("cs5700spring2014 HELLO 001101264\n");
-        // Get an input file handle from the socket and read the input
-        InputStream s1In = s1.getInputStream();
-        // System.out.println("Scoekt Address: " + s1.getRemoteSocketAddress());
-        // System.out.println("Socket is connected: " + s1.isConnected());
-        // System.out.println("Socket available: " + s1In.available());
-        BufferedReader br =
-                new BufferedReader(
-                    new InputStreamReader(s1.getInputStream()));
-        StringBuilder sb = new StringBuilder();
-		String line;
-		while ((line = br.readLine()) != null) {
-				sb.append(line);
-		}
+    private static final String host = "";    //Put the host name here.
+    private static final int port = ;      //Put the port number here.
+    private static final String nuID = "";     //Put your nuID here.
 
-        System.out.println(sb);
+    public static void main(String args[]) throws IOException {
+        // Create the Socket with server.
+        Socket serverSocket = new Socket(host, port);
+        // Create a PrintWriter to write to Server.
+        PrintWriter out = new PrintWriter(serverSocket.getOutputStream(), true);
+        // Create a BufferedReader to get the response from Server.
+        BufferedReader in = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+        out.println("cs5700spring2014 HELLO " + nuID);
+        String response;
+        while (true){
+            response = in.readLine();
+//            System.out.println(response);
+            if (response == null || !response.contains("STATUS"))
+                break;
+            else 
+                out.println(getSolution(response));
+        }
+        System.out.println("Final response with secret flag: " + response);
         // When done, just close the connection and exit
-        br.close();
-        s1In.close();
-        s1.close();
-   }
+        out.close();
+        in.close();
+        serverSocket.close();
+    }
+
+    /**
+     * This method returns the solution in STATUS message.
+     */
+    private static String getSolution(String response){
+        String[] messages = response.split(" ");
+        int numberOfMessages = messages.length;
+        int firstOperand = Integer.parseInt(messages[numberOfMessages - 3]);
+        int secondOperand = Integer.parseInt(messages[numberOfMessages - 1]);
+        String operator = messages[numberOfMessages - 2];
+        long result = getResult(firstOperand, operator, secondOperand);
+//        System.out.println(result);
+        return "cs5700spring2014 " + String.valueOf(result);
+    }
+
+    /**
+     * Given two operands and the operator, this method will return the math result.
+     */
+    private static long getResult(int firstOperand, String operator, int secondOperand){
+        long result = 0;
+        if (operator.equals("+")) {
+            result = firstOperand + secondOperand;
+        } else if (operator.equals("-")) {
+            result = firstOperand - secondOperand;
+        } else if (operator.equals("*")) {
+            result = firstOperand * secondOperand;
+        } else if (operator.equals("/")) {
+            result = firstOperand / secondOperand;
+        } 
+        return result;
+    }
 }
