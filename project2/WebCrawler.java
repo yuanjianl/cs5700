0f71c533deb839181fbb0b7e8d90bab95e08fe31
zhemin <forevermzm00@gmail.com>
2014-09-29 22:12:22 -0400

    Fixed a small bug

--- project2/WebCrawler.java
+++ project2/WebCrawler.java
@@ -169,127 +169,128 @@ public class WebCrawler {
                 System.out.println(flag);
             }
         }
+    }
 
-        public String getCookie() {
-            return headerCookie;
-        }
+    public String getCookie() {
+        return headerCookie;
+    }
 
-        /**
-         * The request is an abstract of Post and Get method. If data
-         * is not null, use Post method, otherwise the Get method.
-         * @param host The host of the website
-         * @param path The path of the request
-         * @param data the request body
-         */
-        public void request(String host, String path, String cookie, String data) {
-            // Use the Get method if data is null or empty.
-            if (data == null || data.equals("")) {
-                out.println("GET " + path + " HTTP/1.1");
-                out.println("Host: " + host);
-                out.println("Connection: keep-alive");
-                if (cookie != null) {
-                    out.println("Cookie: " + cookie);
-                }
-                out.println("User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36");
-                out.println("");
-                out.flush();
-            } else {
-                out.println("POST " + path + " HTTP/1.1");
-                out.println("Host: " + host);
-                out.println("Connection: keep-alive");
+    /**
+     * The request is an abstract of Post and Get method. If data
+     * is not null, use Post method, otherwise the Get method.
+     * @param host The host of the website
+     * @param path The path of the request
+     * @param data the request body
+     */
+    public void request(String host, String path, String cookie, String data) {
+        // Use the Get method if data is null or empty.
+        if (data == null || data.equals("")) {
+            out.println("GET " + path + " HTTP/1.1");
+            out.println("Host: " + host);
+            out.println("Connection: keep-alive");
+            if (cookie != null) {
                 out.println("Cookie: " + cookie);
-                out.println("User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36");
-                out.println("Content-Length: " + data.length());
-                out.println("Content-Type: application/x-www-form-urlencoded; charset=utf-8");
-                out.println("");
-                out.println(data);
-                out.flush();
             }
-
+            out.println("User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36");
+            out.println("");
+            out.flush();
+        } else {
+            out.println("POST " + path + " HTTP/1.1");
+            out.println("Host: " + host);
+            out.println("Connection: keep-alive");
+            out.println("Cookie: " + cookie);
+            out.println("User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36");
+            out.println("Content-Length: " + data.length());
+            out.println("Content-Type: application/x-www-form-urlencoded; charset=utf-8");
+            out.println("");
+            out.println(data);
+            out.flush();
         }
-
-        public void login(String path, String username, String password) {
-            // Initial GET request to /accounts/login/.
-            request(website, path, null, null);
-            String response = read();
-                // System.out.println(response);
-
-                // POST request to /accounts/login/ to log in for cookie and sessionid.
-                String cookie = matchPattern(response, COOKIE_PATTERN).get(0);
-            String sessionid = matchPattern(response, SESSIONID_PATTERN).get(0);
-            String data = "username=" + username + "&password=" + password + "&csrfmiddlewaretoken=" + cookie;
-            String headerCookie = "csrftoken=" + cookie + "; sessionid=" + sessionid;
-            request(website, path, headerCookie, data);
-            response = read();
-
-            // The server will return a new session_id if login successful.
-            sessionid = matchPattern(response, SESSIONID_PATTERN).get(0);
-
-            // This header cookie should be used
-            this.headerCookie = "csrftoken=" + cookie + "; sessionid=" + sessionid;
-        }
-
-        public List<String> matchPattern(String response, String pattern) {
-            Pattern r = Pattern.compile(pattern);
-            Matcher m = r.matcher(response);
-            List<String> results = new ArrayList<String>();
-            while (m.find()) {
-                String t = m.group(1);
-                results.add(t);
-            }
-            return results;
-        }
-
-        public String read() {
-            String t;
-            StringBuilder sb = new StringBuilder();
-            try {
-                // The server would hang 5 seconds before closing the socket. To skip this, we
-                // add a in.ready() to check if server has more things to send.
-                while (true) {
-                    t = in.readLine();
-                    // System.out.println(t);
-                    sb.append(t);
-                    if (!in.ready())
-                        break;
-                }
-            } catch (IOException e) {
-                System.out.println(e);
-                System.out.println("There is an error in reading the response from server.");
-            }
-            return sb.toString();
-        }
-
-        public void finish() throws IOException {
-            in.close();
-            out.close();
-            socket.close();
-        }
-
-        public static void main(String[] args) throws IOException {
-            if (args.length != 4) {
-                System.err.println("Usage: java WebCrawler <host name> <path> <username> <passsword>");
-                System.exit(1);
-            }
-            String website = args[0];
-            String path = args[1];
-            String username = args[2];
-            String password = args[3];
-
-
-            try {
-                WebCrawler crawler = new WebCrawler(website);
-
-                crawler.login("/accounts/login/", username, password);
-
-                crawler.start(path);
-
-                crawler.finish();
-            } catch (NullPointerException ex) {
-                System.err.println("Null Pointer Exception. Exiting");
-                ex.printStackTrace();
-            }
-        }
-
 
     }
+
+    public void login(String path, String username, String password) {
+        // Initial GET request to /accounts/login/.
+        request(website, path, null, null);
+        String response = read();
+        // System.out.println(response);
+
+        // POST request to /accounts/login/ to log in for cookie and sessionid.
+        String cookie = matchPattern(response, COOKIE_PATTERN).get(0);
+        String sessionid = matchPattern(response, SESSIONID_PATTERN).get(0);
+        String data = "username=" + username + "&password=" + password + "&csrfmiddlewaretoken=" + cookie;
+        String headerCookie = "csrftoken=" + cookie + "; sessionid=" + sessionid;
+        request(website, path, headerCookie, data);
+        response = read();
+
+        // The server will return a new session_id if login successful.
+        sessionid = matchPattern(response, SESSIONID_PATTERN).get(0);
+
+        // This header cookie should be used
+        this.headerCookie = "csrftoken=" + cookie + "; sessionid=" + sessionid;
+    }
+
+    public List<String> matchPattern(String response, String pattern) {
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(response);
+        List<String> results = new ArrayList<String>();
+        while (m.find()) {
+            String t = m.group(1);
+            results.add(t);
+        }
+        return results;
+    }
+
+    public String read() {
+        String t;
+        StringBuilder sb = new StringBuilder();
+        try {
+            // The server would hang 5 seconds before closing the socket. To skip this, we
+            // add a in.ready() to check if server has more things to send.
+            while (true) {
+                t = in.readLine();
+                // System.out.println(t);
+                sb.append(t);
+                if (!in.ready())
+                    break;
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+            System.out.println("There is an error in reading the response from server.");
+        }
+        return sb.toString();
+    }
+
+    public void finish() throws IOException {
+        in.close();
+        out.close();
+        socket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 4) {
+            System.err.println("Usage: java WebCrawler <host name> <path> <username> <passsword>");
+            System.exit(1);
+        }
+        String website = args[0];
+        String path = args[1];
+        String username = args[2];
+        String password = args[3];
+
+
+        try {
+            WebCrawler crawler = new WebCrawler(website);
+
+            crawler.login("/accounts/login/", username, password);
+
+            crawler.start(path);
+
+            crawler.finish();
+        } catch (NullPointerException ex) {
+            System.err.println("Null Pointer Exception. Exiting");
+            ex.printStackTrace();
+        }
+    }
+
+
+}
