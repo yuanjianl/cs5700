daea1b7ace3ff52be5101fc1c340646dca77c354
Zhemin Mi <ecemzm@gmail.com>
2014-11-25 14:37:33 -0500

    Init project5.

---
+++ project5/dns.py
@@ -0,0 +1,30 @@
+import SocketServer
+
+replica_hosts = ["54.174.6.90"]
+
+class MyTCPHandler(SocketServer.BaseRequestHandler):
+    """
+    The RequestHandler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print "{} wrote:".format(self.client_address[0])
+        print self.data
+        # just send back the same data, but upper-cased
+        self.request.sendall( replica_hosts[0] )
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 49999
+
+    # Create the server, binding to localhost on port 9999
+    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
