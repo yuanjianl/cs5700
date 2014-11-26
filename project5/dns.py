4caf9ba6836311399de1bd4ebc1208e92d660767
Zhemin Mi <ecemzm@gmail.com>
2014-11-26 18:13:16 -0500

    Adding dns server

--- project5/dns.py
+++ project5/dns.py
@@ -1,30 +1,99 @@
+import sys
 import SocketServer
+import struct
+import socket
 
-replica_hosts = ["54.174.6.90"]
+class Packet():
+    def buildPacket( self, ip ):
+        self.an_count = 1
+        self.flags = 0x8180
 
-class MyTCPHandler(SocketServer.BaseRequestHandler):
+        header = struct.pack('>HHHHHH', self.id, self.flags,
+                             self.qd_count, self.an_count,
+                             self.ns_count, self.ar_count)
+
+        query = ''.join(chr(len(x)) + x for x in self.q_name.split('.'))
+        query += '\x00'  # add end symbol
+        query_part =  query + struct.pack('>HH', self.q_type, self.q_class)
+
+        an_name = 0xC00C
+        an_type = 0x0001
+        an_class = 0x0001
+        an_ttl = 60  # time to live
+        an_len = 4
+        answer_part = struct.pack( '>HHHLH4s', an_name, an_type, an_class,
+                          an_ttl, an_len, socket.inet_aton( ip ) );
+
+        packet = header + query_part + answer_part
+
+        return packet
+
+    def unpackPacket( self, data ):
+        [self.id,
+        self.flags,
+        self.qd_count,
+        self.an_count,
+        self.ns_count,
+        self.ar_count] = struct.unpack('>HHHHHH', data[ :12 ])
+
+        query_data = data[ 12: ]
+        [self.q_type, self.q_class] = struct.unpack('>HH', query_data[-4:])
+        s = query_data[:-4]
+        ptr = 0
+        temp = []
+        while True:
+            count = ord( s[ ptr ] )
+            if count == 0:
+                break
+            ptr += 1
+            temp.append(s[ ptr:ptr + count])
+            ptr += count
+        self.q_name = '.'.join(temp)
+        print "DEBUG: " + self.q_name
+
+class MyDNSHandler(SocketServer.BaseRequestHandler):
     """
-    The RequestHandler class for our server.
-
+    The DNSHandler class for our server.
     It is instantiated once per connection to the server, and must
     override the handle() method to implement communication to the
     client.
     """
-
     def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print "{} wrote:".format(self.client_address[0])
-        print self.data
-        # just send back the same data, but upper-cased
-        self.request.sendall( replica_hosts[0] )
+        data = self.request[0].strip()
+        sock = self.request[1]
 
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 49999
+        packet = Packet()
+        packet.unpackPacket(data)
+        
+        if packet.q_type == 1 and packet.q_name == self.server.name:
+            print "DEBUG: Should reply to: " + str(self.client_address)
+            ip = select_best_replica( self.client_address )
+            response = packet.buildPacket( ip )
 
-    # Create the server, binding to localhost on port 9999
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+            sock.sendto(response, self.client_address)
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    server.serve_forever()
+def select_best_replica( client_address ):
+    return "54.174.6.90"
+
+
+class DNSServer( SocketServer.UDPServer ):
+    def __init__( self, name, port, handler = MyDNSHandler):
+        self.name = name
+        SocketServer.UDPServer.__init__(self, ( '', port ), handler)
+        return
+
+def getPortAndName( argv ):
+    if len( argv ) != 5 or argv[ 1 ] != "-p" or argv[ 3 ] != "-n":
+        sys.exit( "Usage: ./dnsserver -p [port] -s [name]" )
+    port = int( argv[ 2 ] )
+    name = argv[ 4 ]
+
+    return port, name
+
+def main(argv):
+    port, name = getPortAndName( argv )
+    dns_server = DNSServer( name, port )
+    dns_server.serve_forever()
+
+if __name__ == '__main__':
+    main(sys.argv)
